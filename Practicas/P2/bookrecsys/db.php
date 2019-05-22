d0275a412051347a0f6239c685c00060629e44f6
David Gil Bautista <davidgilbautista@gmail.com>
2019-05-22 13:01:18 +0200

    xd no funca

--- Practicas/P2/bookrecsys/db.php
+++ Practicas/P2/bookrecsys/db.php
@@ -1,14 +1,18 @@
 <?php
-/*
-Author: Javed Ur Rehman
-Website: http://www.allphptricks.com/
-*/
-// include 'configuracion.php';
-//
-// $con = mysqli_connect(DB_DSN,DB_USUARIO,DB_CONTRASENIA,DB_NAME);
-// // Check connection
-// if (mysqli_connect_errno())
-//   {
-//   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-//   }
+
+  define("DB_DSN", "localhost");
+  define("DB_NAME", "db45925324_pw1819");
+  define("DB_USUARIO", "x45925324" );
+  define("DB_CONTRASENIA", "45925324" );
+  define("TABLA_USUARIO", "usuario" );
+  define("TABLA_LIBRO", "libro" );
+  define("TABLA_LIBRO_VALORADO", "libro_valorado" );
+
+  $con = mysqli_connect(DB_DSN,DB_USUARIO,DB_CONTRASENIA,DB_NAME);
+
+  // Check connection
+  if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
 ?>
--- Practicas/P2/bookrecsys/registration.php
+++ Practicas/P2/bookrecsys/registration.php
@@ -15,20 +15,8 @@ Website: http://www.allphptricks.com/
 <body>
 
 <?php
-	require('db.php');
-	require_once('configuracion.php');
-	require('datosObject.class.inc');
 
-	try {
-		$conexion = new PDO( DB_DSN, DB_USUARIO, DB_CONTRASENIA );
-		// Se permite a PHP que mantenga la conexión MySQL abierta para
-		// que se emplee en otras partes de la aplicación.
-		$conexion->setAttribute( PDO::ATTR_PERSISTENT, true );
-		$conexion->setAttribute(PDO::ATTR_ERRMODE,
-		PDO::ERRMODE_EXCEPTION );
-	} catch ( PDOException $e ) {
-		die( "Conexión fallida: " . $e->getMessage() );
-	}
+require_once('db.php');
 
     // If form submitted, insert values into the database.
     if (isset($_REQUEST['username'])){
